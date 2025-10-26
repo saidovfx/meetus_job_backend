6a67c213cf2c176ac21e4771a4f566accbec6b06
SeningGitHubUsername <SeningEmail@github.com>
2025-10-26 22:36:30 +0500

    something that I need to merge latest code  to main

--- routes/registerMeetUsUser.js
+++ routes/registerMeetUsUser.js
@@ -112,7 +112,7 @@ router.post("/login", [
     const user = await User.findOne({ username });
     if (!user) return res.status(404).json({ warning: "Foydalanuvchi topilmadi" });
 
-    const isMatch = await bcrypt.compare(password, user.password);    
+    const isMatch =  bcrypt.compare(password, user.password);    
     if (!isMatch) return res.status(401).json({ warning: "Parol noto'g'ri" });
     res.status(200).json({
       success: "Kirish muvaffaqiyatli",
