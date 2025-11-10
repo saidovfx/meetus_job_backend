cf9597f821f3568f1b9b41fd6375a4e898d9bc4c
SeningGitHubUsername <SeningEmail@github.com>
2025-11-10 17:03:10 +0500

    dd

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use("/api/token", tokenRoutes);
 app.use("/api/user", linkRoute);
 app.use("/api/auth", authGoogle);
 app.use("/resume", resume);
-//slom
+//sloms
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDb Connected"))
