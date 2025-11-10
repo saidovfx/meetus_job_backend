4902a915dc6f439b4a499aea1358a273e104dc60
SeningGitHubUsername <SeningEmail@github.com>
2025-11-10 16:32:53 +0500

    hello

--- utils/passport.js
+++ utils/passport.js
@@ -8,7 +8,7 @@
         {
             clientID:process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'https://meetus-job.onrender.com/api/auth/google/callback',
+            callbackURL: 'https://meetus-cjpj.onrender.com/api/auth/google/callback',
         },
         async(accessToken,refreshToken,profile,done)=>{
             try {
